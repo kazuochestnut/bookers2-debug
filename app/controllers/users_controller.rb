5816c28f9c42553a221741642acddeccd2c476da
EC2 Default User <ec2-user@ip-172-31-76-84.ec2.internal>
2020-12-16 13:48:25 +0000

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+    @user =current_user
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+       redirect_to user_path(@user)
+       flash[:notice]="You have created book successfully."
+    else
+       render :edit
+       flash[:notice]="Failed to save"
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find([:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+end
+
